77f7c61a68eeb811a9782193219a330bd4c8285c
macropusgiganteus <phanupong.inchan67@gmail.com>
2022-07-23 22:54:29 +0700

    Add card component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Card from "./Card";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import { robots } from "./robots.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      {robots.map((robot) => (
+        <Card id={robot.id} name={robot.name} email={robot.email} />
+      ))}
+    </div>
   </React.StrictMode>
 );
 
